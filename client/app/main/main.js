f94a935e61451d07a7d26c267653cef88f09317b
iwanaga <iwanaga@iij.ad.jp>
2016-04-03 19:19:42 +0900

    generated by generator-fullstack

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('metAppApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
