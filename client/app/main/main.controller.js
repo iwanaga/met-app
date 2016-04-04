09ce535141922ac994890cda16440a58a61b1344
iwanaga <iwanaga@iij.ad.jp>
2016-04-04 17:39:36 +0900

    グラフ化

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,32 +4,120 @@
 
 class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-    this.awesomeThings = [];
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
+  constructor($scope, thermoHygro) {
+    thermoHygro.query(function (response) {
+      $scope.thermoHygro = [
+        {
+          key: 'temperature',
+          values: response.map(function(d) {
+            return {
+              createdAt: d.createdAt,
+              value: d.temperature
+            };
+          }),
+          color: '#CCC'
+        },
+        {
+          key: '体感温度',
+          values: response.map(function (d) {
+            return {
+              createdAt: d.createdAt,
+              value: d.heatIndex
+            };
+          }),
+          color: '#2196f3'
+        }
+      ];
+      $scope.humidity = [
+        {
+          key: '湿度',
+          values: response.map(function (d) {
+            return {
+              createdAt: d.createdAt,
+              value: d.humidity
+            };
+          }),
+          color: '#2196f3'
+        }
+      ];
     });
-  }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      this.socket.syncUpdates('thing', this.awesomeThings);
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
+    $scope.temperatureOptions = {
+      chart: {
+        type: 'lineChart',
+        height: 400,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 40,
+          left: 55
+        },
+        x: function(d) { return Date.parse(d.createdAt); },
+        y: function(d) { return d.value; },
+        useInteractiveGuideline: false,
+        dispatch: {},
+        xAxis: {
+          axisLabel: '時刻',
+          tickFormat: function (d) {
+            return d3.time.format('%H:%M')(new Date(d))
+          },
+          showMaxMin: false
+        },
+        yAxis: {
+          axisLabel: '温度',
+          axisLabelDistance: 30,
+          showMaxMin: false
+        },
+        forceY: [null, null],
+        transitionDuration: 250,
+        callback: angular.noop
+      },
+      title: {
+        enable: false,
+        text: '温度'
+      },
+      caption: {
+        enable: false
+      }
+    };
+    $scope.humidityOptions = {
+      chart: {
+        type: 'lineChart',
+        height: 400,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 40,
+          left: 55
+        },
+        x: function(d) { return Date.parse(d.createdAt); },
+        y: function(d) { return d.value; },
+        useInteractiveGuideline: false,
+        dispatch: {},
+        xAxis: {
+          axisLabel: '時刻',
+          tickFormat: function (d) {
+            return d3.time.format('%H:%M')(new Date(d))
+          },
+          showMaxMin: false
+        },
+        yAxis: {
+          axisLabel: '湿度',
+          axisLabelDistance: 30,
+          showMaxMin: false
+        },
+        forceY: [null, null],
+        transitionDuration: 250,
+        callback: angular.noop
+      },
+      title: {
+        enable: false,
+        text: '湿度'
+      },
+      caption: {
+        enable: false
+      }
+    };
   }
 }
 
--- client/app/thermoHygro/thermoHygro.service.js
+++ client/app/thermoHygro/thermoHygro.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('metAppApp.thermoHygro')
+angular.module('metAppApp')
   .service('thermoHygro', function ($resource) {
-    return $resource('/api/thermoHygros/:id', {id: @_id});
+    return $resource('/api/thermoHygros/:id', {id: '@_id'});
   });
